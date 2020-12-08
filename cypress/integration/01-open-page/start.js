cc321e7d9a1f10839c91718545c9403fdd067839
Dusana Trifkovic <aleksa2601@gmail.com>
2020-12-08 17:53:22 +0100

    Changed 3 files

--- cypress/integration/01-open-page/start.js
+++ cypress/integration/01-open-page/start.js
@@ -1,3 +1,12 @@
-it('visits todoMVC app', () => {
-  
+/// <reference types="cypress" />
+it("visits todoMVC app", () => {
+  cy.visit("localhost:3000");
+});
+
+it("finds the h1 tag", () => {
+  cy.findByRole("h1");
+});
+
+it("finds an input", () => {
+  cy.get("#add-todo");
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,4 @@
-import "@testing-library/cypress/add-commands";
-
 Cypress.Commands.add("addTodo", (title) => {
   cy.get(".new-todo").type(title + "{enter}");
 });
+import "@testing-library/cypress/add-commands";
